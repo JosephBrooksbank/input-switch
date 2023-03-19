063a2e5410bf3943d944ab5ce85ebcab6aeba5f5
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2023-03-19 13:35:40 -0500

    Add rppal; test gpio functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+
+const BUTTON_PIN: u8 = 27;
+const INPUT_STATUS_PIN: u8 = 4;
+const OUTPUT_SWITCH_PIN: u8 = 17;
+const IR_TRANSMITTER_PIN: u8 = 23;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!(
+        "Switching input on board on a {}.",
+        DeviceInfo::new()?.model()
+    );
+
+    let mut pin = Gpio::new()?.get(OUTPUT_SWITCH_PIN)?.into_output();
+    loop {
+        pin.set_low();
+        thread::sleep(Duration::from_millis(1000));
+        pin.set_high();
+    }
+
+    Ok(())
 }
