89132bacecf33d75a48a7d5801fa759a8750794c
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2023-04-10 18:23:18 -0500

    output detection (disabled for now)
    this code is now functionally deployed to work irl :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::error::Error;
+use std::thread;
+use std::time::Duration;
 
 use rppal::gpio::Gpio;
 use rppal::gpio::Level;
+use rppal::gpio::OutputPin;
 use rppal::gpio::Trigger;
 use rppal::system::DeviceInfo;
-use std::time::{SystemTime};
+use std::time::SystemTime;
 
 const BUTTON_PIN: u8 = 17;
 
@@ -12,27 +15,57 @@ const SHORT_BUTTON_LIMIT: u16 = 250;
 const NOISE_LIMITER: u8 = 1;
 const DEBOUNCE_TIME: u8 = 20;
 const DOUBLE_CLICK_TIMEOUT: u16 = 400;
+
+const OUTPUT_SWITCH_PIN: u8 = 27;
+// const INPUT_STATUS_PIN: u8 = 27;
 // const INPUT_STATUS_PIN: u8 = 4;
-// const OUTPUT_SWITCH_PIN: u8 = 17;
 // const IR_TRANSMITTER_PIN: u8 = 23;
 
 enum ButtonPressType {
     Short,
-    Long
+    Long,
 }
 
-fn button_callback(_: Level) {
-    println!("Button Pressed!");
+// #[derive(Debug)]
+// enum OutputType {
+//     Desktop,
+//     Laptop,
+// }
+
+fn button_callback(level: Level) {
+    if level == Level::High {
+        println!("Input is now on laptop");
+    } else if level == Level::Low {
+        println!("Input is now on desktop");
+    }
 }
+
+fn switch_input(pin: &mut OutputPin) {
+    pin.set_low();
+    thread::sleep(Duration::from_millis(1000));
+    pin.set_high();
+}
+
+// fn get_current_output(pin: &mut InputPin) -> OutputType {
+//     if pin.is_high() {
+//         OutputType::Laptop
+//     } else {
+//         OutputType::Desktop
+//     }
+// }
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!(
         "Switching input on board on a {}.",
         DeviceInfo::new()?.model()
     );
-    let mut pin = Gpio::new()?.get(BUTTON_PIN)?.into_input_pullup();
+    let mut button_pin = Gpio::new()?.get(BUTTON_PIN)?.into_input_pullup();
+    let mut switch_pin = Gpio::new()?.get(OUTPUT_SWITCH_PIN)?.into_output();
+    // let mut status_pin = Gpio::new()?.get(INPUT_STATUS_PIN)?.into_input();
 
-    let res = pin.set_async_interrupt(Trigger::Disabled, button_callback);
+    // status_pin.set_async_interrupt(Trigger::Both, button_callback);
+
+    let res = button_pin.set_async_interrupt(Trigger::Disabled, button_callback);
     match res {
         Ok(_) => (),
         Err(e) => println!("Error: {e}"),
@@ -41,10 +74,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut button_up: SystemTime = SystemTime::now();
     let mut sequence_start: SystemTime = SystemTime::now();
     let mut last_state: Level = Level::High;
-    let mut button_presses: Vec<ButtonPressType> =  Vec::new();
+    let mut button_presses: Vec<ButtonPressType> = Vec::new();
 
     loop {
-        if pin.is_low() {
+        if button_pin.is_low() {
             if last_state == Level::High {
                 if sequence_start.elapsed()?.as_millis() > DOUBLE_CLICK_TIMEOUT.into() {
                     sequence_start = SystemTime::now();
@@ -54,13 +87,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 last_state = Level::Low;
             }
         }
-        if pin.is_high() {
+        if button_pin.is_high() {
             if last_state == Level::Low {
                 if button_up.elapsed()?.as_millis() > DEBOUNCE_TIME.into() {
                     match button_down.elapsed() {
                         Ok(elapsed) => {
                             let milisecs = elapsed.as_millis();
-                            if milisecs > NOISE_LIMITER.into() && milisecs < SHORT_BUTTON_LIMIT.into() {
+                            if milisecs > NOISE_LIMITER.into()
+                                && milisecs < SHORT_BUTTON_LIMIT.into()
+                            {
                                 button_presses.push(ButtonPressType::Short)
                             } else if milisecs > SHORT_BUTTON_LIMIT.into() {
                                 button_presses.push(ButtonPressType::Long);
@@ -75,25 +110,27 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
             last_state = Level::High;
         }
-        if button_presses.len() > 0 && sequence_start.elapsed()?.as_millis() > DOUBLE_CLICK_TIMEOUT.into() {
+        if button_presses.len() > 0
+            && sequence_start.elapsed()?.as_millis() > DOUBLE_CLICK_TIMEOUT.into()
+        {
             let mut short_presses = 0;
             let mut long_presses = 0;
 
             for press in button_presses {
                 match press {
-                    ButtonPressType::Short => {short_presses+=1;}
-                    ButtonPressType::Long => {long_presses+=1;}
+                    ButtonPressType::Short => {
+                        short_presses += 1;
+                    }
+                    ButtonPressType::Long => {
+                        long_presses += 1;
+                    }
                 }
             }
-            
+
             if short_presses == 1 {
-                println!("Short click!");
-            }
-            if short_presses == 2 {
-                println!("Double click!");
-            }
-            if long_presses > 0 {
-                println!("Long press");
+                switch_input(&mut switch_pin);
+            } else if short_presses == 2 {
+            } else if long_presses > 0 {
             }
 
             button_presses = Vec::new();
